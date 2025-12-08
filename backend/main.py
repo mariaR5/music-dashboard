ddd8a397159c66f5a16b76fbadd97eaea8b971f1
romo05 <ro.maria5969@gmail.com>
2025-12-08 21:15:19 +0530

    Added stats endpoints for top songs, artists and total plays

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,26 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, Depends
 from sqlmodel import SQLModel, Session, create_engine, Field, select
+from sqlalchemy import func
 from typing import Optional, List
 from datetime import datetime, timezone
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Load secrets from .env
+load_dotenv()
+
+# print("---------------------------------")
+# print(f"DEBUG: SPOTIPY_CLIENT_ID: {os.getenv("SPOTIPY_CLIENT_ID")}")
+# print(f"DEBUG: SPOTIPY_CLIENT_SECRET: {os.getenv("SPOTIPY_CLIENT_SECRET")}")
+# print("---------------------------------")
+
+# Create spotify client
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id= os.getenv("SPOTIPY_CLIENT_ID"),
+    client_secret= os.getenv("SPOTIPY_CLIENT_SECRET"),
+))
 
 # Setup the database
 sqllite_file_name = "music.db" # database name
@@ -20,6 +39,52 @@ class Scrobble(SQLModel, table=True):
     # Time server recieved data
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
+    # Spotify data
+    spotify_id: Optional[str] = None
+    image_url: Optional[str] = None
+    tempo: Optional[float] = None
+    valence: Optional[float] = None
+    energy: Optional[float] = None
+    genres: Optional[str] = None
+
+# Documentation : https://developer.spotify.com/documentation/web-api
+def enrich_data(title: str, artist: str):
+    print(f"Searching Spotify for {title} - {artist}")
+
+    try:
+        # Search for the track on spotify
+        query = f"track:{title} artist:{artist}" # Spotify query
+        results = sp.search(q=query, type="track", limit=1) # Returns track type dictonary with top search
+
+        items = results["tracks"]["items"]
+
+        # Return empty dictionary if song is not found (ie; items contains nothing)
+        if not items:
+            print("Song not found on Spotify")
+            return {}
+        
+        track = items[0] # Gets the first and only dict in items list
+        t_id = track["id"] # Stores spotify id of the track
+
+        # features = sp.audio_features([t_id])[0] # Deprecated
+
+        artist_id = track["artists"][0]["id"]
+        artist_info = sp.artist(artist_id)
+        genre_list = artist_info["genres"] # Returns a list of genres
+
+        return {
+            "spotify_id": t_id,
+            "image_url": track["album"]["images"][0]["url"],
+            "tempo": None, # features["tempo"],
+            "valence": None, # features["valence"],
+            "energy": None, # features["energy"],
+            "genres": ", ".join(genre_list) # Convert list to string
+        }
+
+    except Exception as e:
+        print(f"Error talking to Spotify: {e}")
+        return {}
+
 
 # Initialise a server
 app = FastAPI()
@@ -34,19 +99,37 @@ def get_session():
     with Session(engine) as session:
         yield session
 
+# Handle request endpoint
+class ScrobbleRequest(SQLModel):
+    title: str
+    artist: str
+    package: str
+    timestamp: int    
+
 # Create POST endpoint (reciever)
 @app.post("/scrobble")
-async def receive_scrobble(scrobble: Scrobble, session: Session = Depends(get_session)): # Dependancy Injection
-    print(f"Saving: {scrobble.title} by {scrobble.artist}")
+async def receive_scrobble(req: Scrobble, session: Session = Depends(get_session)): # Dependancy Injection
+    print(f"Recieved: {req.title} by {req.artist}")
+
+    spotify_data = enrich_data(req.title, req.artist)
+
+    new_scrobble = Scrobble(
+        title=req.title,
+        artist=req.artist,
+        package=req.package,
+        timestamp=req.timestamp,
+        # Unpack spotify data 
+        **spotify_data
+    )
 
     # Save to database
-    session.add(scrobble)
+    session.add(new_scrobble)
     session.commit()
-    session.refresh(scrobble)
+    session.refresh(new_scrobble)
 
     return {
         "status": "success",
-        "id": scrobble.id
+        "data": new_scrobble
     }
 
 # See history : Defines http GET endpoint
@@ -56,6 +139,53 @@ def read_history(session: Session = Depends(get_session)):
         select(Scrobble).order_by(Scrobble.id.desc())
     ).all()
     return scrobbles
+
+# Get top 5 songs
+@app.get("/stats/top-songs")
+def get_top_songs(session: Session = Depends(get_session)):
+    # Select title, artist, image_url, count(id) as plays from Scrobble 
+    # group by title, artist, img_url
+    # order by plays desc
+    # limit 5
+    query = (
+        select(Scrobble.title, Scrobble.artist, Scrobble.image_url, func.count(Scrobble.id).label("plays"))
+        .group_by(Scrobble.title, Scrobble.artist, Scrobble.image_url)
+        .order_by(func.count(Scrobble.id).desc())
+        .limit(5)
+    )
+    result = session.exec(query).all() # Returns list of top 5 songs
+
+    return [
+        {"title": row.title, "artist": row.artist, "img_url": row.image_url, "plays": row.plays}
+        for row in result
+    ]
+
+# Get top 5 artists
+@app.get("/stats/top-artists")
+def get_top_artists(session: Session = Depends(get_session)):
+    query = (
+        select(Scrobble.artist, func.count(Scrobble.id).label("plays"))
+        .group_by(Scrobble.artist)
+        .order_by(func.count(Scrobble.id).desc())
+        .limit(5)
+    )
+
+    results = session.exec(query).all()
+
+    return [
+        {"artist": row.artist, "plays": row.plays}
+        for row in results
+    ]
+
+# Get total plays
+@app.get("/stats/total")
+def get_total_plays(session: Session = Depends(get_session)):
+    query = select(func.count(Scrobble.id))
+
+    result = session.exec(query).one()
+    return {"total_plays": result}
+
+
 
 
 # Health check
